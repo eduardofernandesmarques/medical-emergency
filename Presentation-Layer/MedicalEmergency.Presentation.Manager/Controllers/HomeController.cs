2608ff7b5cd80d526e717bc74734261ab619f5e0
eduardofernandesmarques <eduardo.fernandes@huia.com.br>
2018-08-27 05:20:32 -0300

    Finalizado rotas.

--- Presentation-Layer/MedicalEmergency.Presentation.Manager/Controllers/HomeController.cs
+++ Presentation-Layer/MedicalEmergency.Presentation.Manager/Controllers/HomeController.cs
@@ -1,7 +1,10 @@
-﻿using MedicalEmergency.Domain.Interfaces.Repositories;
+﻿using MedicalEmergency.Domain.Entities;
+using MedicalEmergency.Domain.Interfaces.Repositories;
 using MedicalEmergency.Infrastructure.Data.Repository;
+using MedicalEmergency.Presentation.Manager.Models.Home;
 using System;
 using System.Globalization;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace MedicalEmergency.Presentation.Manager.Controllers
@@ -17,14 +20,17 @@ namespace MedicalEmergency.Presentation.Manager.Controllers
         // GET: Home
         public ActionResult Index(string Latitude, string Longitude)
         {
-            var list = _healthUnitRepository.GetAll();
+            ViewBag.Latitude = Latitude;
+            ViewBag.Longitude = Longitude;
+
+            var list = _healthUnitRepository.GetAll().Select(x => new HealthUnitViewModel { Name = x.Name, Address = x.Address, Latitude = x.Latitude.Replace(",", "."), Longitude = x.Longitude.Replace(",", "."), Phone = x.Phone, ShowRoute = false }).ToList();
 
             if (Latitude != null && Longitude != null)
             {
                 double latitude = ((Math.PI * Convert.ToDouble(Latitude, CultureInfo.InvariantCulture)) / 180);
                 double longitude = ((Math.PI * Convert.ToDouble(Longitude, CultureInfo.InvariantCulture)) / 180);
 
-                list = _healthUnitRepository.DitanceReorder(latitude, longitude);
+                list = _healthUnitRepository.DitanceReorder(latitude, longitude).Select(x => new HealthUnitViewModel { Name = x.Name, Address = x.Address, Latitude = x.Latitude.Replace(",", "."), Longitude = x.Longitude.Replace(",", "."), Phone = x.Phone, ShowRoute = true, Distance = x.Distance }).ToList();
 
                 return View(list);
             }
---
+++ Presentation-Layer/MedicalEmergency.Presentation.Manager/Models/Home/HealthUnitViewModel.cs
@@ -0,0 +1,9 @@
+﻿using MedicalEmergency.Domain.Entities;
+
+namespace MedicalEmergency.Presentation.Manager.Models.Home
+{
+    public class HealthUnitViewModel : HealthUnit
+    {
+        public bool ShowRoute { get; set; }
+    }
+}
